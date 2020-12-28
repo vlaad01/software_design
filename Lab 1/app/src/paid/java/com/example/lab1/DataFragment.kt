cd25ba014f186da1518b6898942947f3c9248983
vlaad01 <vlad.grudovik@gmail.com>
2020-12-29 02:22:05 +0300

    Product flavors fix

--- Lab 1/app/src/paid/java/com/example/lab1/DataFragment.kt
+++ Lab 1/app/src/paid/java/com/example/lab1/DataFragment.kt
@@ -14,15 +14,7 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import com.example.lab1.databinding.FragmentDataBinding
 
-class DataFragment : Fragment() {
-    private var binding: FragmentDataBinding? = null
-    private lateinit var converter: Converter
-    private val viewModel: DataViewModel by activityViewModels()
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+class DataFragment : BaseDataFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,80 +25,13 @@ class DataFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentDataBinding.bind(view)
-        converter = viewModel.converter
-        viewModel.inputText.observe(viewLifecycleOwner, Observer<String> {
-            binding!!.textInput.text = it.toString()
-        })
-        viewModel.outputText.observe(viewLifecycleOwner, Observer<String> {
-            binding!!.textOutput.text = it.toString()
-        })
-        viewModel.dataSwapped.observe(viewLifecycleOwner, Observer<Boolean> {
-            binding!!.spInput.setSelection(converter.inputId)
-            binding!!.spOutput.setSelection(converter.outputId)
-        })
+        binding!!.buttonCopyInput.setOnClickListener{
+            copyToClipboard(binding!!.textInput.text.toString())
+        }
 
-        binding!!.buttonCopyInput.setOnClickListener {
-            copyToClipboard(binding!!.textInput.text.toString())}
         binding!!.buttonCopyOutput.setOnClickListener{
             copyToClipboard(binding!!.textOutput.text.toString())
         }
-        binding!!.spCategory.adapter = getAdapter(converter.categories)
-        binding!!.spInput.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                converter.inputId = 0
-            }
-
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                converter.inputId = position
-            }
-        }
-        binding!!.spOutput.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                converter.outputId = 0
-            }
-
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                converter.outputId = position
-            }
-        }
-        binding!!.spCategory.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                converter.categoryId = position
-                binding!!.spInput.adapter = getAdapter(converter.getUnitList())
-                binding!!.spOutput.adapter = getAdapter(converter.getUnitList())
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                converter.categoryId = 0
-                binding!!.spInput.adapter = getAdapter(converter.getUnitList())
-                binding!!.spOutput.adapter = getAdapter(converter.getUnitList())
-            }
-        }
-    }
-
-    private fun getAdapter(list: List<String>) : ArrayAdapter<String> {
-        val activity = activity
-        return ArrayAdapter<String>(
-            activity as Context,
-            R.layout.support_simple_spinner_dropdown_item,
-            list
-        )
     }
 
     private fun copyToClipboard(text: String) {
--- Lab 1/app/src/paid/java/com/example/lab1/KeyboardFragment.kt
+++ Lab 1/app/src/paid/java/com/example/lab1/KeyboardFragment.kt
@@ -12,12 +12,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
 import com.example.lab1.databinding.FragmentKeyboardBinding
 
-class KeyboardFragment : Fragment(R.layout.fragment_keyboard) {
-
-    private var binding: FragmentKeyboardBinding? = null
-    private val viewModel: DataViewModel by activityViewModels()
-    private var floatNum: Boolean = false
-    private lateinit var converter: Converter
+class KeyboardFragment : BaseKeyboardFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,53 +27,7 @@ class KeyboardFragment : Fragment(R.layout.fragment_keyboard) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentKeyboardBinding.bind(view)
-        converter = viewModel.converter
-        viewModel.inputText.value = ""
-        viewModel.outputText.value = ""
-        binding!!.button0.setOnClickListener { editInput("0", false) }
-        binding!!.button1.setOnClickListener { editInput("1", false) }
-        binding!!.button2.setOnClickListener { editInput("2", false) }
-        binding!!.button3.setOnClickListener { editInput("3", false) }
-        binding!!.button4.setOnClickListener { editInput("4", false) }
-        binding!!.button5.setOnClickListener { editInput("5", false) }
-        binding!!.button6.setOnClickListener { editInput("6", false) }
-        binding!!.button7.setOnClickListener { editInput("7", false) }
-        binding!!.button8.setOnClickListener { editInput("8", false) }
-        binding!!.button9.setOnClickListener { editInput("9", false) }
-        binding!!.buttonDot.setOnClickListener { editInput(".", true) }
-        binding!!.buttonBackspace.setOnClickListener { backSpace() }
-        binding!!.buttonClear.setOnClickListener { clear() }
         binding!!.buttonSwap.setOnClickListener { swap() }
-    }
-
-    private fun editInput(string: String, dot: Boolean) {
-        if (dot && floatNum) {
-            return
-        } else if (dot) {
-            floatNum = true
-        }
-        viewModel.inputText.value += string
-        viewModel.outputText.value = viewModel.inputText.value?.let { converter.convert(it) }
-    }
-
-    private fun backSpace() {
-        var string = viewModel.inputText.value
-        if (string!!.isNotEmpty()) {
-            if (string[string.length - 1] == '.') {
-                floatNum = false
-            }
-            string = string.substring(0, string.length - 1)
-            viewModel.inputText.value = string
-            viewModel.outputText.value = converter.convert(string)
-        }
-        return
-    }
-
-    private fun clear() {
-        viewModel.inputText.value = ""
-        viewModel.outputText.value = ""
-        floatNum = false
     }
 
     private fun swap() {
